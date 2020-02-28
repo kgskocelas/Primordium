08efe92ecc9aad3685528262296794bf22a72371
Charles Ofria <charles.ofria@gmail.com>
2020-02-28 12:42:45 -0500

    Eliminated config object; put vars right in World.

--- SpatialRestraint4.cc
+++ SpatialRestraint4.cc
@@ -26,8 +26,13 @@ struct Organism {
   }
 };
 
-/// Information needed to configure a run.
-struct Config {
+struct World {
+  emp::Random random;
+  emp::SettingCombos combos;
+  emp::vector<Organism> orgs;
+  std::set<Organism> org_set; // Organisms waiting to replicate.
+  double time = 0.0;
+
   size_t cells_side = 32;  ///< How many cells are on a side of the (square) multi-cell?
   size_t time_range = 1.0; ///< Replication takes 100.0 + a random value up to time_range.
   size_t neighbors = 8;    ///< Num neighbors to consider for offspring (0=well mixed; 4,6,8 => 2D)
@@ -36,48 +41,17 @@ struct Config {
   size_t start_1s = 5;     ///< How many ones in the starting organism?
   double mut_prob = 0.0;   ///< Probability of an offspring being mutated.
 
-  bool Set(emp::SettingCombos & combos) {
-    cells_side = combos.GetValue<size_t>("cells_side");
-    time_range = combos.GetValue<size_t>("time_range");
-    neighbors = combos.GetValue<size_t>("neighbors");
-    bit_size = combos.GetValue<size_t>("bit_size");
-    restrain = combos.GetValue<size_t>("restrain");
-    start_1s = combos.GetValue<size_t>("start_1s");
-    mut_prob = combos.GetValue<double>("mut_prob");
-
-    // If any setting combinations are impossible, reject.
-    if (start_1s > bit_size) return false;
-
-    return true;
-  }
-
-  size_t GetWidth() const { return cells_side; }
-  size_t GetHeight() const { return cells_side; }
-  size_t GetSize() const { return cells_side * cells_side; }
-
-  size_t ToPos(size_t x, size_t y) const { return x + y * cells_side; }
-  size_t ToX(size_t pos) const { return pos % cells_side; }
-  size_t ToY(size_t pos) const { return pos / cells_side; }
-};
-
-struct World {
-  emp::Random random;
-  emp::SettingCombos combos;
-  emp::vector<Organism> orgs;
-  std::set<Organism> org_set; // Organisms waiting to replicate.
-  double time = 0.0;
-  Config config;
   bool verbose = false;
 
   World(int argc, char* argv[]) {
-    combos.AddSetting<size_t>("time_range", "Rep time = 100.0 + random(time_range)", "-t") = { 50 };
-    combos.AddSetting<size_t>("neighbors",  "Neighborhood size for replication", "-n") = { 8 };
-    combos.AddSetting<size_t>("cells_side", "Cells on side of (square) multicell", "-c") = { 16 };
+    combos.AddSetting<size_t>("time_range", "Rep time = 100.0 + random(time_range)", "-t", time_range) = { 50 };
+    combos.AddSetting<size_t>("neighbors",  "Neighborhood size for replication", "-n", neighbors) = { 8 };
+    combos.AddSetting<size_t>("cells_side", "Cells on side of (square) multicell", "-c", cells_side) = { 16 };
+    combos.AddSetting<size_t>("bit_size",   "Number of bits in genome?", "-b", bit_size) = { 10 };
+    combos.AddSetting<size_t>("restrain",   "Num ones in genome for restraint?", "-r", restrain) = { 5 };
+    combos.AddSetting<size_t>("start_1s",   "How many 1s in starting organism?", "-1", start_1s) = { 5 };
+    combos.AddSetting<double>("mut_prob",   "Probability of mutation in offspring", "-m", mut_prob) = { 0.0 };
     combos.AddSetting<size_t>("data_count", "Number of times to replicate each run", "-d") = { 100 };
-    combos.AddSetting<size_t>("bit_size",   "Number of bits in genome?", "-b") = { 10 };
-    combos.AddSetting<size_t>("restrain",   "Num ones in genome for restraint?", "-r") = { 5 };
-    combos.AddSetting<size_t>("start_1s",   "How many 1s in starting organism?", "-1") = { 5 };
-    combos.AddSetting<double>("mut_prob",   "Probability of mutation in offspring", "-m") = { 0.0 };
 
     emp::vector<std::string> args = emp::cl::args_to_strings(argc, argv);
 
@@ -100,6 +74,14 @@ struct World {
     }
   }
 
+  size_t GetWidth() const { return cells_side; }
+  size_t GetHeight() const { return cells_side; }
+  size_t GetSize() const { return cells_side * cells_side; }
+
+  size_t ToPos(size_t x, size_t y) const { return x + y * cells_side; }
+  size_t ToX(size_t pos) const { return pos % cells_side; }
+  size_t ToY(size_t pos) const { return pos / cells_side; }
+
   // Convert a resource count to a character.
   static constexpr char ToChar(size_t count) {
     if (count < 10) return '0' + count;
@@ -118,17 +100,17 @@ struct World {
 
   size_t RandomNeighbor(size_t pos)
   {
-    if (config.neighbors == 0 || config.neighbors > 8) {
-      return random.GetUInt(config.GetSize());
+    if (neighbors == 0 || neighbors > 8) {
+      return random.GetUInt(GetSize());
     }
 
-    const size_t x = config.ToX(pos);
-    const size_t y = config.ToY(pos);
+    const size_t x = ToX(pos);
+    const size_t y = ToY(pos);
     size_t next_x = (size_t) -1;
     size_t next_y = (size_t) -1;
 
-    while (next_x >= config.GetWidth() || next_y >= config.GetHeight()) {
-      const size_t dir = random.GetUInt(config.neighbors);  // Direction for offspring.
+    while (next_x >= GetWidth() || next_y >= GetHeight()) {
+      const size_t dir = random.GetUInt(neighbors);  // Direction for offspring.
       switch (dir) {
       case 0: case 5: case 7: next_x = x-1; break;
       case 1: case 4: case 6: next_x = x+1; break;
@@ -141,17 +123,17 @@ struct World {
       };
     }
 
-    emp_assert(config.ToPos(next_x, next_y) < config.GetSize(), next_x, next_y, config.cells_side);
+    emp_assert(ToPos(next_x, next_y) < GetSize(), next_x, next_y, cells_side);
 
-    return config.ToPos(next_x, next_y);
+    return ToPos(next_x, next_y);
   }
 
   // Print current resource levels in population.
   void Print() {
-    emp_assert(orgs.size() == config.GetSize());
+    emp_assert(orgs.size() == GetSize());
     size_t pos = 0;
-    for (size_t y = 0; y < config.GetHeight(); y++) {
-      for (size_t x = 0; x < config.GetWidth(); x++) {
+    for (size_t y = 0; y < GetHeight(); y++) {
+      for (size_t x = 0; x < GetWidth(); x++) {
         if (orgs[pos].repro_time == 0.0) std::cout << " -";
       	else std::cout << " " << ToChar(orgs[pos].num_ones);
         pos++;
@@ -161,15 +143,15 @@ struct World {
   }
 
   void SetupOrg(Organism & org) {
-    org.repro_time = time + 100.0 + random.GetDouble(config.time_range);
+    org.repro_time = time + 100.0 + random.GetDouble(time_range);
     org_set.insert(org);
   }
 
   void DoBirth(Organism & offspring, const Organism & parent, bool do_mutations=true) {
     // Setup the new offspring, possibly with mutations.
     offspring.num_ones = parent.num_ones;
-    if (do_mutations && random.P(config.mut_prob)) {
-      double prob1 = ((double) offspring.num_ones) / (double) config.bit_size;
+    if (do_mutations && random.P(mut_prob)) {
+      double prob1 = ((double) offspring.num_ones) / (double) bit_size;
       if (random.P(prob1)) offspring.num_ones--;
       else offspring.num_ones++;
     }
@@ -180,7 +162,7 @@ struct World {
   
   double TestMulticell() {
     // Setup initial multicell to be empty; keep count of resources in each cell.
-    const size_t mc_size = config.GetSize();
+    const size_t mc_size = GetSize();
     orgs.resize(0);         // Clear out any current organisms.
     orgs.resize(mc_size);   // Put in new organsims initialized to 0.
     for (size_t id = 0; id < mc_size; id++) {
@@ -188,11 +170,12 @@ struct World {
       orgs[id].repro_time = 0.0;
     }
     org_set.clear();
+    time = 0.0;
 
     // Inject a cell in the middle.
-    const size_t start_pos = config.ToPos(config.GetWidth()/2, config.GetHeight()/2);
+    const size_t start_pos = ToPos(GetWidth()/2, GetHeight()/2);
     Organism & inject_org = orgs[start_pos]; // Find the org position to inject.
-    inject_org.num_ones = config.start_1s;   // Initialize injection to proper default;
+    inject_org.num_ones = start_1s;   // Initialize injection to proper default;
     SetupOrg(inject_org);                    // Do any extra setup for this organism.
 
     // Loop through updates until cell is full.
@@ -218,7 +201,7 @@ struct World {
       }
 
       // Otherwise if we don't restrain, reset existing organism there.
-      else if (next_org.num_ones < config.restrain) {
+      else if (next_org.num_ones < restrain) {
         org_set.erase(next_org);
         DoBirth(next_org, parent);
       }
@@ -239,9 +222,9 @@ struct World {
     os << ", ave_time" << std::endl;
 
     // Loop through configuration combonations to test.
+    combos.Reset();
     do {
       os << combos.CurString(", ");  // Output current setting combination data.
-      config.Set(combos);        // Store current setting combination in config object.      
 
       // Conduct all replicates and output the information.    
       double total_time = 0.0;
