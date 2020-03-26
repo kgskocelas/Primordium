08a3d6b3e397e0eb3369903bbc72995ecdc106be
Charles Ofria <charles.ofria@gmail.com>
2020-03-25 23:34:20 -0400

    Separated out into simple header and code files.

---
+++ SpatialRestraint.cc
@@ -0,0 +1,22 @@
+/**
+ *  @copyright Copyright (C) Michigan State University, MIT Software license; see doc/LICENSE.md
+ *  @date 2020.
+ *
+ *  @file  SpatialRestraint.cc
+ *  @brief Examining the evolution of cellular restraint in a multi-cell
+ *  @note Status: BETA
+ *
+ *  Cells are either RESTRAINED or UNRESTRAINED
+ *  Organisms are "multi-cells" that start as a single cell and automatically replicate when full.
+ *  Cells replicate within the multicell when they have collected enough resource.
+ *  - Restrained cells replicate into an empty neighbor position (or fail if no empty)
+ *  - Unrestrained cells replicate into a random position, regardless of if anything is there.
+ */
+
+#include "SpatialRestraint.h"
+
+int main(int argc, char* argv[])
+{
+  World world(argc, argv);
+  world.Run(std::cout);
+}
