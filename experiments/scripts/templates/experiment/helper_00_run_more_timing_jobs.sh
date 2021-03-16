126bbd1d5ec5135f102f8dc769c65115e56ad94a
Austin Ferguson <ferguson.austin.j@gmail.com>
2021-03-16 15:59:11 -0400

    Add soma sweep chapter files

--- experiments/scripts/templates/experiment/helper_00_run_more_timing_jobs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Load in config options
-source ./config.sh
-
-${SR_TIMING_DIR}/roll_q/roll_q.sh
--- experiments/scripts/templates/experiment/helper_02_run_more_evo_jobs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Load in config options
-source ./config.sh
-
-${SR_EVO_DIR}/roll_q/roll_q.sh
