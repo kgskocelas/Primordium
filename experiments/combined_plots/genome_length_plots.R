1ad3e007bdfea4808dcdcc34f25fcd34f5aadeda
Austin Ferguson <ferguson.austin.j@gmail.com>
2022-05-19 15:40:28 -0400

    Update specific plots to include stats markings

--- experiments/combined_plots/genome_length_plots.R
+++ experiments/combined_plots/genome_length_plots.R
@@ -7,6 +7,7 @@ library(ggridges)
 library(scales)
 library(khroma)
 library(cowplot)
+library(ggsignif)
 
 # Load the data
 df = read.csv('../2021_02_27__genome_length/evolution/data/scraped_evolution_data_length_50.csv')
@@ -49,12 +50,53 @@ color_map = c(
 text_major_size = 18
 text_minor_size = 16 
 
+# Run the stats necessary for these plots (rest are in supplement)
+size_vec = c(16, 32, 64, 128, 256, 512)
+y_position_even = 35
+y_position_odd = 40
+adjacent_counter = 0
+df_test = df2
+df_wilcox = data.frame(data = matrix(nrow = 0, ncol = 7))
+colnames(df_wilcox) = c('size_a', 'size_b', 'p_value_corrected', 'p_value_raw', 'W', 'is_adjacent', 'y_position')
+for(size_idx_a in 1:(length(size_vec) - 1)){
+  size_a = size_vec[size_idx_a]
+  size_a_str = paste0(size_a, 'x', size_a)
+  for(size_idx_b in (size_idx_a + 1):length(size_vec)){
+    size_b = size_vec[size_idx_b]
+    size_b_str = paste0(size_b, 'x', size_b)
+    res = wilcox.test(df_test[df_test$MCSIZE == size_a,]$restraint_value, df_test[df_test$MCSIZE == size_b,]$restraint_value, alternative = 'two.sided') 
+    is_adjacent = (size_b == size_a * 2)
+    y_position = y_position_even
+    if(is_adjacent == T){
+      if(adjacent_counter %% 2 != 0){
+        y_position = y_position_odd
+      }
+      adjacent_counter = adjacent_counter + 1
+    }
+    df_wilcox[nrow(df_wilcox) + 1,] = c(size_a_str, size_b_str, 0, res$p.value, as.numeric(res$statistic)[1], is_adjacent, as.numeric(y_position))
+  }
+}
+df_wilcox$p_value_corrected = p.adjust(df_wilcox$p_value_raw, method = 'holm')
+df_wilcox$less_0.01 = df_wilcox$p_value_corrected < 0.01
+df_wilcox$label = 'ns'
+df_wilcox[df_wilcox$p_value_corrected <= 0.05,]$label = '*'
+df_wilcox[df_wilcox$p_value_corrected <= 0.01,]$label = '**'
+df_wilcox[df_wilcox$p_value_corrected <= 0.001,]$label = '***'
+df_wilcox$size_a_factor = factor(df_wilcox$size_a, levels = c('16x16', '32x32', '64x64', '128x128', '256x256', '512x512'))
+df_wilcox$size_b_factor = factor(df_wilcox$size_b, levels = c('16x16', '32x32', '64x64', '128x128', '256x256', '512x512'))
+print(df_wilcox)
 
 # x-axis = organism size
 # y-axis = average evolved restraint buffer for each replicate
 ggp_400_bit = ggplot(df2[df2$generation == 10000 & df2$LENGTH == 400,], aes(x = size_factor, y = restraint_value)) +
   geom_hline(aes(yintercept = 0), alpha = 0.5, linetype = 'dashed') +
   geom_boxplot(aes(fill = size_factor)) +
+  geom_signif(
+    data = df_wilcox[df_wilcox$is_adjacent == T,],
+    aes(annotations = label, xmin=size_a_factor, xmax=size_b_factor,y_position=as.numeric(y_position)),
+    manual=T,
+    inherit.aes=FALSE
+  ) +
   xlab('Organism size') +
   ylab('Evolved restraint buffer') +
   scale_fill_manual(values = color_map) +
@@ -70,7 +112,45 @@ ggp_400_bit = ggplot(df2[df2$generation == 10000 & df2$LENGTH == 400,], aes(x = 
   theme(legend.text = element_text(size = text_minor_size)) +
   theme(strip.text = element_text(size = text_minor_size, color = '#000000')) +
   theme(strip.background = element_rect(fill = '#dddddd')) +
-  scale_y_continuous(limits=c(-42, 33))
+  scale_y_continuous(limits=c(-42, 40))
+ggp_400_bit
+
+
+# Run the stats necessary for these plots (rest are in supplement)
+size_vec = c(16, 32, 64, 128, 256, 512)
+y_position_even = 35
+y_position_odd = 40
+adjacent_counter = 0
+df_test = df2
+df_wilcox = data.frame(data = matrix(nrow = 0, ncol = 7))
+colnames(df_wilcox) = c('size_a', 'size_b', 'p_value_corrected', 'p_value_raw', 'W', 'is_adjacent', 'y_position')
+for(size_idx_a in 1:(length(size_vec) - 1)){
+  size_a = size_vec[size_idx_a]
+  size_a_str = paste0(size_a, 'x', size_a)
+  for(size_idx_b in (size_idx_a + 1):length(size_vec)){
+    size_b = size_vec[size_idx_b]
+    size_b_str = paste0(size_b, 'x', size_b)
+    res = wilcox.test(df_test[df_test$MCSIZE == size_a,]$restraint_value, df_test[df_test$MCSIZE == size_b,]$restraint_value, alternative = 'two.sided') 
+    is_adjacent = (size_b == size_a * 2)
+    y_position = y_position_even
+    if(is_adjacent == T){
+      if(adjacent_counter %% 2 != 0){
+        y_position = y_position_odd
+      }
+      adjacent_counter = adjacent_counter + 1
+    }
+    df_wilcox[nrow(df_wilcox) + 1,] = c(size_a_str, size_b_str, 0, res$p.value, as.numeric(res$statistic)[1], is_adjacent, as.numeric(y_position))
+  }
+}
+df_wilcox$p_value_corrected = p.adjust(df_wilcox$p_value_raw, method = 'holm')
+df_wilcox$less_0.01 = df_wilcox$p_value_corrected < 0.01
+df_wilcox$label = 'ns'
+df_wilcox[df_wilcox$p_value_corrected <= 0.05,]$label = '*'
+df_wilcox[df_wilcox$p_value_corrected <= 0.01,]$label = '**'
+df_wilcox[df_wilcox$p_value_corrected <= 0.001,]$label = '***'
+df_wilcox$size_a_factor = factor(df_wilcox$size_a, levels = c('16x16', '32x32', '64x64', '128x128', '256x256', '512x512'))
+df_wilcox$size_b_factor = factor(df_wilcox$size_b, levels = c('16x16', '32x32', '64x64', '128x128', '256x256', '512x512'))
+print(df_wilcox)
 
 # x-axis = genome length
 # y-axis = average evolved restraint buffer for each replicate
@@ -92,8 +172,8 @@ ggp_size_256 = ggplot(df2[df2$generation == 10000 & df2$MCSIZE == 256,], aes(x =
   theme(legend.text = element_text(size = text_minor_size)) +
   theme(strip.text = element_text(size = text_minor_size, color = '#000000')) +
   theme(strip.background = element_rect(fill = '#dddddd')) +
-  scale_y_continuous(limits=c(-42, 33))
-
+  scale_y_continuous(limits=c(-42, 40))
+ggp_size_256
 
 ####################################
 ########## COMBINED ################
--- experiments/combined_plots/infinite_population_plots.R
+++ experiments/combined_plots/infinite_population_plots.R
@@ -4,32 +4,54 @@ rm(list = ls())
 library(ggplot2)
 library(cowplot)
 library(khroma)
+library(ggsignif)
 
-####################################
-############ FINITE ################
-####################################
+
+size_vec = c(16, 32, 64, 128, 256, 512)
+num_samples = 3000
+num_batches = 30 
+batch_size = num_samples / num_batches
+y_position_even = 80 
+y_position_odd = 85
+max_gen = 10000
+
+########################################
+############ FINITE POP ################
+########################################
 
 # Load data
-df_finite = read.csv('../2021_03_07__inf_population/simulation/data/size_16.csv')
-df_finite = rbind(df_finite, read.csv('../2021_03_07__inf_population/simulation/data/size_32.csv'))
-df_finite = rbind(df_finite, read.csv('../2021_03_07__inf_population/simulation/data/size_64.csv'))
-df_finite = rbind(df_finite, read.csv('../2021_03_07__inf_population/simulation/data/size_128.csv'))
-df_finite = rbind(df_finite, read.csv('../2021_03_07__inf_population/simulation/data/size_256.csv'))
-df_finite = rbind(df_finite, read.csv('../2021_03_07__inf_population/simulation/data/size_512.csv'))
+df_finite = NA
+for(size in size_vec){
+  for(batch_id in 1:num_batches){
+    if(!is.data.frame(df_finite)){
+      df_finite = read.csv(paste0('../2021_03_07__inf_population/simulation/data/size_', size, '__batch_', batch_id, '.csv'))
+      df_finite$batch_id = batch_id
+    } else{
+      df_finite_tmp = read.csv(paste0('../2021_03_07__inf_population/simulation/data/size_', size, '__batch_', batch_id, '.csv'))
+      df_finite_tmp$batch_id = batch_id
+      df_finite = rbind(df_finite, df_finite_tmp)
+    }
+  }
+}
 
 # Summarize data, grabbing the mean from the simulation
-size_vec = c(16, 32, 64, 128, 256, 512)
-df_finite_summary = data.frame(data = matrix(nrow = 0, ncol = 3))
-colnames(df_finite_summary) = c('mc_size', 'generation', 'mean_ones')
+df_finite_summary = data.frame(data = matrix(nrow = 0, ncol = 4))
+colnames(df_finite_summary) = c('mc_size', 'generation', 'mean_ones', 'batch_id')
 for(size in size_vec){
-  mask = df_finite$mc_size == size
-  cat('Size: ', size, '\n\t')
-  for(gen in unique(df_finite$generation)){
-    df_finite_summary[nrow(df_finite_summary) + 1,] = c(size, gen, weighted.mean(0:100, df_finite[mask & df_finite$generation == gen,]$frac_of_pop))
-    cat(gen, ' ')
+  size_mask = df_finite$mc_size == size
+  for(batch_id in unique(df_finite$batch_id)){
+    mask = size_mask & df_finite$batch_id == batch_id 
+    cat(paste0('(', batch_id, ') Size: ', size, '\n\t'))
+    for(gen in unique(df_finite$generation)){
+      df_finite_summary[nrow(df_finite_summary) + 1,] = c(size, gen, weighted.mean(0:100, df_finite[mask & df_finite$generation == gen,]$frac_of_pop), batch_id)
+      cat(gen, ' ')
+    }
+    cat('\n')
   }
-  cat('\n')
 }
+
+# Trim to final generation
+df_finite_summary = df_finite_summary[df_finite_summary$generation == max_gen,]
 
 # Create variables to make plotting easier
 df_finite_summary$restraint_value = df_finite_summary$mean_ones - 60
@@ -54,15 +76,56 @@ if(!dir.exists('./plots')){
   dir.create('./plots')
 }
 
+# Calculate stats
+df_finite_wilcox = data.frame(data = matrix(nrow = 0, ncol = 7))
+adjacent_counter = 0
+colnames(df_finite_wilcox) = c('size_a', 'size_b', 'p_value_corrected', 'p_value_raw', 'W', 'is_adjacent', 'y_position')
+for(size_idx_a in 1:(length(size_vec) - 1)){
+  size_a = size_vec[size_idx_a]
+  size_a_str = paste0(size_a, 'x', size_a)
+  for(size_idx_b in (size_idx_a + 1):length(size_vec)){
+    size_b = size_vec[size_idx_b]
+    size_b_str = paste0(size_b, 'x', size_b)
+    res = wilcox.test(
+      df_finite_summary[df_finite_summary$mc_size == size_a,]$restraint_value, 
+      df_finite_summary[df_finite_summary$mc_size == size_b,]$restraint_value, 
+      alternative = 'two.sided') 
+    is_adjacent = (size_b == size_a * 2)
+    y_position = y_position_even
+    if(is_adjacent == T){
+      if(adjacent_counter %% 2 != 0){
+        y_position = y_position_odd
+      }
+      adjacent_counter = adjacent_counter + 1
+    }
+    df_finite_wilcox[nrow(df_finite_wilcox) + 1,] = c(size_a_str, size_b_str, 0, res$p.value, as.numeric(res$statistic)[1], is_adjacent, as.numeric(y_position))
+  }
+}
+df_finite_wilcox$p_value_corrected = p.adjust(df_finite_wilcox$p_value_raw, method = 'holm')
+df_finite_wilcox$less_0.01 = df_finite_wilcox$p_value_corrected < 0.01
+df_finite_wilcox$label = 'ns'
+df_finite_wilcox[df_finite_wilcox$p_value_corrected <= 0.05,]$label = '*'
+df_finite_wilcox[df_finite_wilcox$p_value_corrected <= 0.01,]$label = '**'
+df_finite_wilcox[df_finite_wilcox$p_value_corrected <= 0.001,]$label = '***'
+df_finite_wilcox$size_a_factor = factor(df_finite_wilcox$size_a, levels = c('16x16', '32x32', '64x64', '128x128', '256x256', '512x512'))
+df_finite_wilcox$size_b_factor = factor(df_finite_wilcox$size_b, levels = c('16x16', '32x32', '64x64', '128x128', '256x256', '512x512'))
+print(df_finite_wilcox)
+
 ggp_finite = ggplot(df_finite_summary[df_finite_summary$generation == 10000,], aes(x = size_factor, y = restraint_value ))  +
   geom_hline(aes(yintercept = 0), alpha = 0.5, linetype = 'dashed') + 
-  geom_col(aes(fill = size_factor)) + 
-  geom_text(aes(x = size_factor, y = restraint_value + 2, label = round(restraint_value, 1))) + 
+  geom_boxplot(aes(fill = size_factor)) + 
+  #geom_text(aes(x = size_factor, y = restraint_value + 2, label = round(restraint_value, 1))) + 
+  geom_signif(
+    data = df_finite_wilcox[df_finite_wilcox$is_adjacent == T,],
+    aes(annotations = label, xmin=size_a_factor, xmax=size_b_factor,y_position=as.numeric(y_position)),
+    manual=T,
+    inherit.aes=FALSE
+  ) +
   xlab('Organism size') + 
   ylab('Average restraint buffer') + 
   labs(color = 'Organism size') + 
   scale_fill_manual(values = color_map) + 
-  scale_y_continuous(limits = c(0, 70)) +
+  scale_y_continuous(limits = c(0, 85)) +
   theme_light() + 
   theme(axis.title = element_text(size = text_major_size)) + 
   theme(axis.text = element_text(size = text_minor_size)) + 
@@ -75,47 +138,101 @@ ggp_finite = ggplot(df_finite_summary[df_finite_summary$generation == 10000,], a
   theme(strip.background = element_rect(fill = '#dddddd')) +
   theme(legend.position = 'none') 
 
+ggp_finite
 
-####################################
-########### INFINITE ###############
-####################################
+########################################
+########### INFINITE POP ###############
+########################################
 
 # Load data
-df_infinite = read.csv('../2021_03_08__inf_genome_inf_pop/simulation/data/size_16.csv')
-df_infinite = rbind(df_infinite, read.csv('../2021_03_08__inf_genome_inf_pop/simulation/data/size_32.csv'))
-df_infinite = rbind(df_infinite, read.csv('../2021_03_08__inf_genome_inf_pop/simulation/data/size_64.csv'))
-df_infinite = rbind(df_infinite, read.csv('../2021_03_08__inf_genome_inf_pop/simulation/data/size_128.csv'))
-df_infinite = rbind(df_infinite, read.csv('../2021_03_08__inf_genome_inf_pop/simulation/data/size_256.csv'))
-df_infinite = rbind(df_infinite, read.csv('../2021_03_08__inf_genome_inf_pop/simulation/data/size_512.csv'))
+df_infinite = NA
+for(size in size_vec){
+  for(batch_id in 1:num_batches){
+    if(!is.data.frame(df_infinite)){
+      df_infinite = read.csv(paste0('../2021_03_08__inf_genome_inf_pop/simulation/data/size_', size, '__batch_', batch_id, '.csv'))
+      df_infinite$batch_id = batch_id
+    } else{
+      df_infinite_tmp = read.csv(paste0('../2021_03_08__inf_genome_inf_pop/simulation/data/size_', size, '__batch_', batch_id, '.csv'))
+      df_infinite_tmp$batch_id = batch_id
+      df_infinite = rbind(df_infinite, df_infinite_tmp)
+    }
+  }
+}
 
 # Summarize data, grabbing the mean from the simulation
-size_vec = c(16, 32, 64, 128, 256, 512)
-df_infinite_summary = data.frame(data = matrix(nrow = 0, ncol = 3))
-colnames(df_infinite_summary) = c('mc_size', 'generation', 'mean_ones')
+df_infinite_summary = data.frame(data = matrix(nrow = 0, ncol = 4))
+colnames(df_infinite_summary) = c('mc_size', 'generation', 'mean_ones', 'batch_id')
 for(size in size_vec){
-  mask = df_infinite$mc_size == size
-  cat('Size: ', size, '\n\t')
-  for(gen in unique(df_infinite$generation)){
-    df_infinite_summary[nrow(df_infinite_summary) + 1,] = c(size, gen, weighted.mean(-100:550, df_infinite[mask & df_infinite$generation == gen,]$frac_of_pop))
-    cat(gen, ' ')
+  size_mask = df_infinite$mc_size == size
+  for(batch_id in unique(df_infinite$batch_id)){
+    mask = size_mask & df_infinite$batch_id == batch_id
+    cat(paste0('(', batch_id, ') Size: ', size, '\n\t'))
+    for(gen in unique(df_infinite$generation)){
+      df_infinite_summary[nrow(df_infinite_summary) + 1,] = c(size, gen, weighted.mean(-100:550, df_infinite[mask & df_infinite$generation == gen,]$frac_of_pop), batch_id)
+      cat(gen, ' ')
+    }
+    cat('\n')
   }
-  cat('\n')
 }
+
+# Trim to final generation
+df_infinite_summary = df_infinite_summary[df_infinite_summary$generation == max_gen,]
 
 # Create variables to make plotting easier
 df_infinite_summary$restraint_value = df_infinite_summary$mean_ones 
 df_infinite_summary$size_str = paste0(df_infinite_summary$mc_size, 'x', df_infinite_summary$mc_size)
 df_infinite_summary$size_factor = factor(df_infinite_summary$size_str, levels = paste0(size_vec, 'x', size_vec))
 
+# Calculate stats
+df_infinite_wilcox = data.frame(data = matrix(nrow = 0, ncol = 7))
+adjacent_counter = 0
+colnames(df_infinite_wilcox) = c('size_a', 'size_b', 'p_value_corrected', 'p_value_raw', 'W', 'is_adjacent', 'y_position')
+for(size_idx_a in 1:(length(size_vec) - 1)){
+  size_a = size_vec[size_idx_a]
+  size_a_str = paste0(size_a, 'x', size_a)
+  for(size_idx_b in (size_idx_a + 1):length(size_vec)){
+    size_b = size_vec[size_idx_b]
+    size_b_str = paste0(size_b, 'x', size_b)
+    res = wilcox.test(
+      df_infinite_summary[df_infinite_summary$mc_size == size_a,]$restraint_value, 
+      df_infinite_summary[df_infinite_summary$mc_size == size_b,]$restraint_value, 
+      alternative = 'two.sided') 
+    is_adjacent = (size_b == size_a * 2)
+    y_position = y_position_even
+    if(is_adjacent == T){
+      if(adjacent_counter %% 2 != 0){
+        y_position = y_position_odd
+      }
+      adjacent_counter = adjacent_counter + 1
+    }
+    df_infinite_wilcox[nrow(df_infinite_wilcox) + 1,] = c(size_a_str, size_b_str, 0, res$p.value, as.numeric(res$statistic)[1], is_adjacent, as.numeric(y_position))
+  }
+}
+df_infinite_wilcox$p_value_corrected = p.adjust(df_infinite_wilcox$p_value_raw, method = 'holm')
+df_infinite_wilcox$less_0.01 = df_infinite_wilcox$p_value_corrected < 0.01
+df_infinite_wilcox$label = 'ns'
+df_infinite_wilcox[df_infinite_wilcox$p_value_corrected <= 0.05,]$label = '*'
+df_infinite_wilcox[df_infinite_wilcox$p_value_corrected <= 0.01,]$label = '**'
+df_infinite_wilcox[df_infinite_wilcox$p_value_corrected <= 0.001,]$label = '***'
+df_infinite_wilcox$size_a_factor = factor(df_infinite_wilcox$size_a, levels = c('16x16', '32x32', '64x64', '128x128', '256x256', '512x512'))
+df_infinite_wilcox$size_b_factor = factor(df_infinite_wilcox$size_b, levels = c('16x16', '32x32', '64x64', '128x128', '256x256', '512x512'))
+print(df_infinite_wilcox)
+
 ggp_infinite = ggplot(df_infinite_summary[df_infinite_summary$generation == 10000,], aes(x = size_factor, y = restraint_value ))  +
   geom_hline(aes(yintercept = 0), alpha = 0.5, linetype = 'dashed') + 
-  geom_col(aes(fill = size_factor)) + 
-  geom_text(aes(x = size_factor, y = restraint_value + 2, label = round(restraint_value, 1))) + 
+  geom_boxplot(aes(fill = size_factor)) + 
+  #geom_text(aes(x = size_factor, y = restraint_value + 2, label = round(restraint_value, 1))) + 
+  geom_signif(
+    data = df_infinite_wilcox[df_infinite_wilcox$is_adjacent == T,],
+    aes(annotations = label, xmin=size_a_factor, xmax=size_b_factor,y_position=as.numeric(y_position)),
+    manual=T,
+    inherit.aes=FALSE
+  ) +
   xlab('Organism size') + 
   ylab('Average restraint buffer') + 
   labs(color = 'Organism size') + 
   scale_fill_manual(values = color_map) + 
-  scale_y_continuous(limits = c(0, 70)) +
+  scale_y_continuous(limits = c(0, 85)) +
   theme_light() + 
   theme(axis.title = element_text(size = text_major_size)) + 
   theme(axis.text = element_text(size = text_minor_size)) + 
@@ -127,6 +244,7 @@ ggp_infinite = ggplot(df_infinite_summary[df_infinite_summary$generation == 1000
   theme(strip.text = element_text(size = text_minor_size, color = '#000000')) + 
   theme(strip.background = element_rect(fill = '#dddddd')) +
   theme(legend.position = 'none')  
+ggp_infinite
 
 ####################################
 ########## COMBINED ################
